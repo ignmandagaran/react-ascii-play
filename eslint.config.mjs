c7a1a20ce3ce3693c383036b595c485d12791bdb
Nacho Mandagaran <ignaciofmandagaran@gmail.com>
2025-04-22 16:07:44 -0300

    Add ESLint configuration and update dependencies for TypeScript and React

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,11 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import * as tseslint from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
 
-export default tseslint.config(
+export default [
   js.configs.recommended,
-  ...tseslint.configs.recommended,
   {
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
@@ -14,6 +14,7 @@ export default tseslint.config(
         ...globals.browser,
         ...globals.es2021,
       },
+      parser: tsParser,
       parserOptions: {
         ecmaVersion: 'latest',
         sourceType: 'module',
@@ -23,10 +24,12 @@ export default tseslint.config(
       },
     },
     plugins: {
+      '@typescript-eslint': tseslint,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
+      ...tseslint.configs.recommended.rules,
       'react-hooks/rules-of-hooks': 'error',
       'react-hooks/exhaustive-deps': 'warn',
       'react-refresh/only-export-components': [
@@ -35,4 +38,4 @@ export default tseslint.config(
       ],
     },
   },
-)
+]
